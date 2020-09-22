7c18b4121dff18f6e2a415bae5a0451ad6097102
AntLoop <antloop@pop-os.localdomain>
2020-09-22 18:12:19 +0300

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ timedatectl set-ntp true
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup # move mirrorlist and make backup
 
 echo "Please enter country: (example Finland(FI), Sweden(SE), United States(US), Germany(GE)...)"
-echo "Multiple countries can be selected using commas (e.g. Finland,Germany or FI,GE) 
+echo "Multiple countries can be selected using commas (e.g. Finland,Germany or FI,GE)" 
 read COUNTRY
-reflector --verbose --country ${COUNTRY} --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose --latest 5 --country ${COUNTRY} --sort rate --save /etc/pacman.d/mirrorlist
+
+echo -e "/nIntalling prereqs.../n$HR"
