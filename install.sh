08b4156630117b9f14456e1461572170bd0f88cb
AntLoop <antloop@pop-os.localdomain>
2020-09-22 17:29:10 +0300

    new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#*************************************
+#* Setup and install script for Arch *
+#*************************************
+
+echo "**************************************************"
+echo "* Setting mirrors for optimal download - Finland *"
+echo "**************************************************"
+
+timedatectl set-ntp true
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup # move mirrorlist and make backup
+
+echo "Please enter country: (example Finland(FI), Sweden...)"
+read COUNTRY
+reflector --verbose --country ${COUNTRY} --sort rate --save /etc/pacman.d/mirrorlist
