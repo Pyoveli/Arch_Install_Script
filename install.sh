65078c7b778e702acf58561bb2de66298d35d9a8
Pyoveli <sami.kemppainen1@gmail.com>
2020-09-22 18:26:03 +0000

    modified:   install.sh 	new file:   install_chroot.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,10 @@ echo -e "\nInstalling prereqs...\n"
 echo "*************************"
 echo "* Select disk to format *"
 echo "*************************"
+
 lsblk
+
+echo "Please enter disk: (example: /dev/sda)"
 read DISK
 echo -e "\nFormatting disk...\n"
 
@@ -29,8 +32,8 @@ sgdisk -Z ${DISK}
 sgdisk -2048 -o ${DISK}		# Create new gpt disk 2040 alignment
 
 #Create partitions
-sgdisk -n 1:0:+1G ${DISK}	# Partition 1 (UEFI)
-sgdisk -a 2048 -o ${DISK}	# Partition 2 (Root)
+sgdisk -n 1:0:+1024M ${DISK}	# Partition 1 (UEFI)
+sgdisk -n 2:0:0 ${DISK}		# Partition 2 (Root)
 
 #Set partition types
 sgdisk -t 1:ef00 ${DISK}
@@ -39,4 +42,30 @@ sgdisk -t 2:8300 ${DISK}
 #Label partitions
 sgdisk -c 1:"UEFISYS" ${DISK}
 sgdisk -c 2:"ROOT" ${DISK}
-#
+
+#Make filesystems
+echo -e "\nCreating Filesystems...\n"
+
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.ext4 -L "ROOT" "${DISK}2"
+
+#Mount targets
+mkdir -v /mnt
+mount -t ext4 "${DISK}2" /mnt
+mkdir -v /mnt/boot
+mkdir -v /mnt/boot/efi
+mount -t vfat "${DISK}1" /mnt/boot/efi
+
+echo "******************************"
+echo "* Arch install on main drive *"
+echo "******************************"
+pacstrap /mnt base base-devel linux linux-firmware vim nano sudo
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp install_chroot.sh /mnt		# Copy script to /mnt to continue in chroot
+arch-chroot /mnt ./install_chroot.sh
+
+rm /mnt/install_chroot.sh		# Remove 2nd install script from /mnt
+umount -R /mnt
+
+echo "*** System ready for 1st Boot ***"
---
+++ install_chroot.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "***********************************"
+echo "* Bootloader systemd installation *"
+echo "***********************************"
+
+bootctl install
+cat <<EOF  > /boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options=root=/dev/sda1 rw   		#remeber to change DISKS
+EOF
+
+echo "*** Network setup ***"
+pacman -S networkmanager dhclient 
+systemctl enable --now NetworkManager
+
+echo "*** Set password for Root ***"
+echo "Enter password for root: "
+passwd root
+
+exit
