961dade12f07582cfb7855540b945fd1ccb85baf
AntLoop <antloop@pop-os.localdomain>
2020-09-22 17:45:06 +0300

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,6 @@ timedatectl set-ntp true
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup # move mirrorlist and make backup
 
 echo "Please enter country: (example Finland(FI), Sweden(SE), United States(US), Germany(GE)...)"
+echo "Multiple countries can be selected using commas (e.g. Finland,Germany or FI,GE) 
 read COUNTRY
 reflector --verbose --country ${COUNTRY} --sort rate --save /etc/pacman.d/mirrorlist
